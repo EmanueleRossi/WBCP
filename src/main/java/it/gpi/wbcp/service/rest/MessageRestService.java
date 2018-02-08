8495472fffc3f2c098e201fd4dedc31064fc8691
mbassetti <marco.bassetti@gpi.it>
2018-02-08 10:04:52 +0100

    fix - message author management

--- src/main/java/it/gpi/wbcp/service/rest/MessageRestService.java
+++ src/main/java/it/gpi/wbcp/service/rest/MessageRestService.java
@@ -55,40 +55,40 @@ import org.apache.logging.log4j.Logger;
 @Path("/message")
 @Stateless
 public class MessageRestService {
-    
+
     private static final Logger logger = LogManager.getLogger();
-		
+
     @EJB
     UserDao userDao;
     @EJB
-    MessageDao messageDao;   
+    MessageDao messageDao;
     @EJB
-    ApplicationParameterDao aParameterDao;    
-	
+    ApplicationParameterDao aParameterDao;
+
     @POST
     @Path("/create")
-    @Produces(MediaType.APPLICATION_JSON)    
-    public Response create(@Context HttpServletRequest httpRequest, 
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(@Context HttpServletRequest httpRequest,
                            @Context User requestUser,
                            Message message) {
         Response response;
         ResourceBundle lmb = ResourceBundle.getBundle("WBCP-web", httpRequest.getLocale());
         try {
-            if (message.getUser() == null | StringUtil.isNullOrEmpty(message.getUser().getEmail())) {              
+            if (message.getUser() == null | StringUtil.isNullOrEmpty(message.getUser().getEmail())) {
                 ApplicationError ae = new ApplicationError(lmb.getString("message.creation.no_user"));
                 logger.warn(ae);
                 response = Response.status(Status.NOT_FOUND).entity(ae).build();
             } else {
-                if (message.getSender() == null | StringUtil.isNullOrEmpty(message.getSender().getEmail())) {              
+                if (message.getSender() == null | StringUtil.isNullOrEmpty(message.getSender().getEmail())) {
                     ApplicationError ae = new ApplicationError(lmb.getString("message.creation.no_sender"));
                     logger.warn(ae);
                     response = Response.status(Status.NOT_FOUND).entity(ae).build();
                 } else {
-                    if (message.getRecipient() == null | StringUtil.isNullOrEmpty(message.getRecipient().getEmail())) {              
+                    if (message.getRecipient() == null | StringUtil.isNullOrEmpty(message.getRecipient().getEmail())) {
                         ApplicationError ae = new ApplicationError(lmb.getString("message.creation.no_recipient"));
                         logger.warn(ae);
                         response = Response.status(Status.NOT_FOUND).entity(ae).build();
-                    } else {                       
+                    } else {
                         User user = userDao.getByEmail(requestUser.getEmail());
                         if (user == null) {
                             ApplicationError ae = new ApplicationError(lmb.getString("message.creation.user_not_registered"));
@@ -99,45 +99,45 @@ public class MessageRestService {
                             if (sender == null) {
                                 ApplicationError ae = new ApplicationError(lmb.getString("message.creation.sender_not_registered"));
                                 logger.warn(ae);
-                                response = Response.status(Status.NOT_FOUND).entity(ae).build();                                
+                                response = Response.status(Status.NOT_FOUND).entity(ae).build();
                             } else {
                                 User recipient = userDao.getByEmail(message.getRecipient().getEmail());
                                 if (recipient == null) {
                                     ApplicationError ae = new ApplicationError(lmb.getString("message.creation.recipient_not_registered"));
                                     logger.warn(ae);
-                                    response = Response.status(Status.NOT_FOUND).entity(ae).build();   
-                                } else {                         
+                                    response = Response.status(Status.NOT_FOUND).entity(ae).build();
+                                } else {
                                     String clearTextPayload = message.getPayload();
-                                    
-                                    message.setUser(user);  
-                                    
+
+                                    message.setUser(user);
+
                                     CryptoUtil cuSender = new CryptoUtil(sender.getPublicKeyBase64());
                                     SecretKey senderAESMessageKey = cuSender.newAESKey();
                                     byte[] senderEncryptedPayload = cuSender.encrypt_AES(clearTextPayload.getBytes(StandardCharsets.UTF_8.name()), senderAESMessageKey);
                                     byte[] senderEncryptedAuthor = cuSender.encrypt_AES(sender.getSignature().getBytes(StandardCharsets.UTF_8.name()), senderAESMessageKey);
-                                    message.setPayload(StringUtil.getBase64EncodedUTF8String(senderEncryptedPayload));  
+                                    message.setPayload(StringUtil.getBase64EncodedUTF8String(senderEncryptedPayload));
                                     message.setAuthor(StringUtil.getBase64EncodedUTF8String(senderEncryptedAuthor));
                                     byte[] cryptedSenderAESMessageKey = cuSender.encrypt_RSA(senderAESMessageKey.getEncoded());
                                     message.setAESKeyRSACryptedBase64(StringUtil.getBase64EncodedUTF8String(cryptedSenderAESMessageKey));
-                                    messageDao.persist(message);                
+                                    messageDao.persist(message);
 
-                                    Message recipientMessage = new Message();     
+                                    Message recipientMessage = new Message();
                                     recipientMessage.setUser(recipient);
                                     recipientMessage.setSender(sender);
-                                    recipientMessage.setRecipient(recipient);                                                                        
+                                    recipientMessage.setRecipient(recipient);
                                     CryptoUtil cuRecipient = new CryptoUtil(recipient.getPublicKeyBase64());
                                     SecretKey recipientAESMessageKey = cuRecipient.newAESKey();
                                     byte[] recipientEncryptedPayload = cuRecipient.encrypt_AES(clearTextPayload.getBytes(StandardCharsets.UTF_8.name()), recipientAESMessageKey);
-                                    byte[] recipientEncryptedAuthor = cuRecipient.encrypt_AES(recipient.getSignature().getBytes(StandardCharsets.UTF_8.name()), recipientAESMessageKey);                                    
+                                    byte[] recipientEncryptedAuthor = cuRecipient.encrypt_AES(sender.getSignature().getBytes(StandardCharsets.UTF_8.name()), recipientAESMessageKey);
                                     recipientMessage.setPayload(StringUtil.getBase64EncodedUTF8String(recipientEncryptedPayload));
                                     recipientMessage.setAuthor(StringUtil.getBase64EncodedUTF8String(recipientEncryptedAuthor));
                                     byte[] cryptedRecipientAESMessageKey = cuRecipient.encrypt_RSA(recipientAESMessageKey.getEncoded());
                                     recipientMessage.setAESKeyRSACryptedBase64(StringUtil.getBase64EncodedUTF8String(cryptedRecipientAESMessageKey));
                                     messageDao.persist(recipientMessage);
-                                    
+
                                     String smtpHost = aParameterDao.getParameterAsString("SMTP_HOST");
                                     Integer smtpPort = aParameterDao.getParameterAsInteger("SMTP_PORT");
-                                    Boolean sslEnabled = aParameterDao.getParameterAsBoolean("SSL_ENABLED");                                
+                                    Boolean sslEnabled = aParameterDao.getParameterAsBoolean("SSL_ENABLED");
                                     String smtpAuthUsername = aParameterDao.getParameterAsString("SMTP_AUTH_USERNAME");
                                     String smtpAuthPassword = aParameterDao.getParameterAsString("SMTP_AUTH_PASSWORD");
                                     String mailFromAddress = aParameterDao.getParameterAsString("MAIL_FROM_ADDRESS");
@@ -149,65 +149,65 @@ public class MessageRestService {
                                    
                                     response = Response.status(Status.OK).entity(message).build();
                                 }
-                            }                            
+                            }
                         }
-                    }    
-                }   
-            }                                
+                    }
+                }
+            }
         } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | InvalidAlgorithmParameterException | IOException e) {
             ApplicationError ae = new ApplicationError(e);
             logger.error("Exception using crypto utility. STACKTRACE=|{}|", StringUtil.stringifyStackTrace(e));
-            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(ae).build();   
+            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(ae).build();
         } catch (Exception eg) {
             ApplicationError ae = new ApplicationError(eg);
             logger.error("Generic exception. STACKTRACE=|{}|", StringUtil.stringifyStackTrace(eg));
-            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(ae).build();   
-        }        
+            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(ae).build();
+        }
         return response;
-    }        
-    
+    }
+
     @POST
     @Path("/searchByInstant/{instantFrom}/{instantTo}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response searchByInstant(@Context HttpServletRequest httpRequest,
-                                    @PathParam("instantFrom") String instantFrom, 
+                                    @PathParam("instantFrom") String instantFrom,
                                     @PathParam("instantTo") String instantTo,
                                     @Context User requestUser) {
         Response response;
         ResourceBundle lmb = ResourceBundle.getBundle("WBCP-web", httpRequest.getLocale());
         Calendar instantFromCalendar = Calendar.getInstance(httpRequest.getLocale());
         Calendar instantToCalendar = Calendar.getInstance(httpRequest.getLocale());
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");        
-        try {                     
-            instantFromCalendar.setTime(sdf.parse(instantFrom));   
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            instantFromCalendar.setTime(sdf.parse(instantFrom));
             instantToCalendar.setTime(sdf.parse(instantTo));
-            User user = userDao.getByEmail(requestUser.getEmail());                                    
-            List<Message> messages = messageDao.searchByInstant(user, instantFromCalendar, instantToCalendar);                       
+            User user = userDao.getByEmail(requestUser.getEmail());
+            List<Message> messages = messageDao.searchByInstant(user, instantFromCalendar, instantToCalendar);
             if (messages == null || messages.isEmpty()) {
                 ApplicationError ae = new ApplicationError(lmb.getString("message.not_found") + String.format("instantFrom=|{}|, instantTo=|{}|", instantFrom, instantTo));
-                logger.trace(ae);                
+                logger.trace(ae);
                 response = Response.status(Status.NOT_FOUND).entity(ae).build();
-            } else {                                                                                           
-                CryptoUtil cu = new CryptoUtil(user.getPublicKeyBase64(), requestUser.getPrivateKeyBase64());    
+            } else {
+                CryptoUtil cu = new CryptoUtil(user.getPublicKeyBase64(), requestUser.getPrivateKeyBase64());
                 for (Message message : messages) {
-                    byte[] aesKeyByteArray = cu.decrypt_RSA(StringUtil.decodeBase64(message.getAESKeyRSACryptedBase64()));                                       
-                    byte[] decodedMessagePayload = cu.decrypt_AES(StringUtil.decodeBase64(message.getPayload()), new SecretKeySpec(aesKeyByteArray, 0, aesKeyByteArray.length, "AES"));                    
-                    message.setPayload(new String(decodedMessagePayload, StandardCharsets.UTF_8.name())); 
-                }                                              
-                response = Response.status(Status.OK).entity(messages).build();                
-            }                   
+                    byte[] aesKeyByteArray = cu.decrypt_RSA(StringUtil.decodeBase64(message.getAESKeyRSACryptedBase64()));
+                    byte[] decodedMessagePayload = cu.decrypt_AES(StringUtil.decodeBase64(message.getPayload()), new SecretKeySpec(aesKeyByteArray, 0, aesKeyByteArray.length, "AES"));
+                    message.setPayload(new String(decodedMessagePayload, StandardCharsets.UTF_8.name()));
+                }
+                response = Response.status(Status.OK).entity(messages).build();
+            }
         } catch (ParseException pe) {
             ApplicationError ae = new ApplicationError(String.format(lmb.getString("message.search_by_instant.invalid_date"), instantFrom, instantTo), pe);
-            logger.warn(ae);                   
+            logger.warn(ae);
             response = Response.status(Status.NOT_FOUND).entity(ae).build();
-        } catch (Exception eg) { 
-            ApplicationError aeg = new ApplicationError(eg);                        
+        } catch (Exception eg) {
+            ApplicationError aeg = new ApplicationError(eg);
             logger.error("Generic exception executing message search. STACKTRACE=|{}|", StringUtil.stringifyStackTrace(eg));
-            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(aeg).build();             
-        }     
-        return response;        
-    }  
-    
+            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(aeg).build();
+        }
+        return response;
+    }
+
     @POST
     @Path("/getAuthor/{messageId}")
     @Produces(MediaType.APPLICATION_JSON)
@@ -216,27 +216,27 @@ public class MessageRestService {
                               @Context User requestUser) {
         Response response;
         ResourceBundle lmb = ResourceBundle.getBundle("WBCP-web", httpRequest.getLocale());
-        try {                     
-            User user = userDao.getByEmail(requestUser.getEmail());                                    
+        try {
+            User user = userDao.getByEmail(requestUser.getEmail());
             Message message = messageDao.getById(messageId);
             if (message == null) {
                 ApplicationError ae = new ApplicationError(lmb.getString("message.not_found") + String.format("messageId=|{}|", messageId));
-                logger.trace(ae);                
+                logger.trace(ae);
                 response = Response.status(Status.NOT_FOUND).entity(ae).build();
-            } else {                                                                                           
-                CryptoUtil cu = new CryptoUtil(user.getPublicKeyBase64(), requestUser.getPrivateKeyBase64());                
-               
-                byte[] aesKeyByteArray = cu.decrypt_RSA(StringUtil.decodeBase64(message.getAESKeyRSACryptedBase64()));                                       
+            } else {
+                CryptoUtil cu = new CryptoUtil(user.getPublicKeyBase64(), requestUser.getPrivateKeyBase64());
+
+                byte[] aesKeyByteArray = cu.decrypt_RSA(StringUtil.decodeBase64(message.getAESKeyRSACryptedBase64()));
                 byte[] decodedMessageAuthor = cu.decrypt_AES(StringUtil.decodeBase64(message.getAuthor()), new SecretKeySpec(aesKeyByteArray, 0, aesKeyByteArray.length, "AES"));
-                message.setAuthor(new String(decodedMessageAuthor, StandardCharsets.UTF_8.name())); 
-                                                              
-                response = Response.status(Status.OK).entity(message).build();                
-            }                   
-        } catch (Exception eg) { 
-            ApplicationError aeg = new ApplicationError(eg);                        
+                message.setAuthor(new String(decodedMessageAuthor, StandardCharsets.UTF_8.name()));
+
+                response = Response.status(Status.OK).entity(message).build();
+            }
+        } catch (Exception eg) {
+            ApplicationError aeg = new ApplicationError(eg);
             logger.error("Generic exception executing getAuthor(). STACKTRACE=|{}|", StringUtil.stringifyStackTrace(eg));
-            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(aeg).build();             
-        }     
-        return response;        
-    }         
+            response = Response.status(Status.INTERNAL_SERVER_ERROR).entity(aeg).build();
+        }
+        return response;
+    }
 }
